b80fe927273e64e22463c975ad76cf3f1b140de2
koorochka <artemij@koorochka.ru>
2018-07-30 17:54:43 +0300

    Simpe load

--- koorochka/file.php
+++ koorochka/file.php
@@ -5,20 +5,26 @@
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 
 
-d($_FILES);
 
 ?>
 
-<form action="" method="post" enctype="multipart/form-data">
-    <?for($i=0; $i<3;$i++):?>
-        <div class="margin-bottom-15">
-            <input type="file"
-                   name="file[]">
-        </div>
-    <?endfor;?>
-    <input type="submit"
-           value="Submit"
-           class="btn btn-danger">
-</form>
+<?
+$arr_file=Array(
+    "name" => $_FILES[IMAGE_ID][name],
+    "size" => $_FILES[IMAGE_ID][size],
+    "tmp_name" => $_FILES[IMAGE_ID][tmp_name],
+    "type" => "",
+    "old_file" => "",
+    "del" => "Y",
+    "MODULE_ID" => "koorochka.sglass");
+$fid = CFile::SaveFile($arr_file, "landings");
+if (strlen($fid)>0):
+    ?><?echo CFile::ShowImage($fid, 200, 200, "border=0", "", true);
+endif;
+?>
+    <form method = "post" enctype = 'multipart/form-data'>
+        <?echo CFile::InputFile("IMAGE_ID", 20, $str_IMAGE_ID);?>
+        <input type="submit" value="Сохранить">
+    </form>
 
 <?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?>
